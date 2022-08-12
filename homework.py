f7bf32602f35083f0f0e0529b901ca7c826e70c1
Андрей Легкий <legkiy.andrey@bk.ru>
2022-08-12 22:06:51 +0300

    проект1

--- homework.py
+++ homework.py
@@ -49,9 +49,7 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        message1 = InfoMessage(self.__class__.__name__, self.duration,
-                               self.get_distance(), self.get_mean_speed(),
-                               self.get_spent_calories())
+        message1 = InfoMessage(self.__class__.__name__, self.duration, self.get_distance(), self.get_mean_speed(), self.get_spent_calories())
         return message1
 
 
@@ -86,21 +84,23 @@ class SportsWalking(Training):
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    coeff_calorie_5 = 1.3
-    coeff_calorie_6 = 2.1
+    coeff_calorie_5 = 1.1
+    coeff_calorie_6 = 2.0
     LEN_STEP: float = 1.38
     def __init__(self, action: int, duration: float, weight: float,
                  length_pool: float, count_pool: int) -> None:
+        super().__init__(action,
+                         duration, weight)
         self.length_pool = length_pool
         self.count_pool = count_pool
+    def get_distance(self) -> float:
+        return (self.action * Swimming.LEN_STEP) / Training.M_IN_KM
     def get_mean_speed(self):
         return (self.length_pool * self.count_pool / Training.M_IN_KM
                 / self.duration)
-
     def get_spent_calories(self) -> float:
         return ((self.get_mean_speed() + self.coeff_calorie_5)
                 * self.coeff_calorie_6 * self.weight)
-
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
     read: dict[str, Training] = {
@@ -112,21 +112,16 @@ def read_package(workout_type: str, data: list) -> Training:
         return None
     read1 = read.get(workout_type)(*data)
     return read1
-
-
 def main(training: Training) -> None:
     """Главная функция."""
     info = training.show_training_info()
     print(info.get_message())
-
-
 if __name__ == '__main__':
     packages = [
         ('SWM', [720, 1, 80, 25, 40]),
         ('RUN', [15000, 1, 75]),
         ('WLK', [9000, 1, 75, 180]),
     ]
-
     for workout_type, data in packages:
         training = read_package(workout_type, data)
         if training is None:
